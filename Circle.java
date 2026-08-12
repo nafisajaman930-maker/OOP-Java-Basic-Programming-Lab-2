7a122043e1feec72d59e7bf2b78e1081a00c4fe8
nafisajaman930-maker <nafisajaman930@gmail.com>
2026-08-12 15:57:12 +0600

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,11 @@
+public class Circle {
+    public static void main(String[] args) {
+        double radius = 7.5;
+
+        double area = 3.1416 * radius * radius;
+        double perimeter = 2 * 3.1416 * radius;
+
+        System.out.println("Area=" + area);
+        System.out.println("Perimeter=" + perimeter);
+    }
+}
